d06047ed54da34e4569a98419625ef299ca0f580
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-04-08 10:28:00 -0300

    fix: changed cors config

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept',
   );
-  res.header('Access-Control-Allow-Origin', 'https://listlucas.netlify.app/');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PATCH, POST, PUT,DELETE');
   app.use(cors());
   next();

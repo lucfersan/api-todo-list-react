823015529e294a275b8c5d9d086dc86f5136afdd
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-04-09 01:59:29 -0300

    refactor: ormconfig dev and prod

--- ormconfig.js
+++ ormconfig.js
@@ -1,24 +1,19 @@
 const mainFolder = process.env.NODE_ENV ? 'dist' : 'src';
 
-let productionConfig = null;
-if (mainFolder === 'dist') {
-  productionConfig = {
-    ssl: true,
-    extra: {
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    },
-  };
-}
-
 module.exports = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
   entities: [`${mainFolder}/app/models/*.js`],
   migrations: [`${mainFolder}/database/migrations/*.js`],
-  synchronize: true,
-  productionConfig,
+  synchronize: process.env.NODE_ENV ? true : false,
+  ssl: process.env.NODE_ENV ? true : false,
+  extra: process.env.NODE_ENV
+    ? {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {},
   cli: {
     migrationsDir: 'src/database/migrations',
   },

8ab59c8eea44d5777c8b918365c8091fb4b0a547
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-04-09 01:37:26 -0300

    refactor: heroku configuration

--- ormconfig.js
+++ ormconfig.js
@@ -1,15 +1,24 @@
+const mainFolder = process.env.NODE_ENV ? 'dist' : 'src';
+
+let productionConfig = null;
+if (mainFolder === 'dist') {
+  productionConfig = {
+    ssl: true,
+    extra: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+  };
+}
+
 module.exports = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  entities: ['dist/app/models/*.js'],
-  migrations: ['dist/database/migrations/*.js'],
+  entities: [`${mainFolder}/app/models/*.js`],
+  migrations: [`${mainFolder}/database/migrations/*.js`],
   synchronize: true,
-  ssl: true,
-  extra: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
+  productionConfig,
   cli: {
     migrationsDir: 'src/database/migrations',
   },
